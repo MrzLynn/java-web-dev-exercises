c7d83d0672ab9f6c736b03cfaa862da460106ca6
Your Name <you@example.com>
2021-04-28 19:28:24 -0500

    Finished studio

---
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+    public Teacher(String firstName, String lastName, String subject, int yearsTeaching) {
+        this.firstName =
+    }
+
+}
